fd7124c44134acf76b95fc9d23dd1cae5d51e75a
danieljonesobot <daniel.jonesobot@gmail.com>
2024-03-25 15:51:17 +0100

    formatted code with prettier

--- neostratify/src/Components/About/About.jsx
+++ neostratify/src/Components/About/About.jsx
@@ -1,79 +1,72 @@
-import React from 'react'
-import { getImageUrl } from '../../utils';
-import styles from './About.module.css';
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./About.module.css";
 
 const About = () => {
   return (
-    <section className={styles.container} id='about'>
-        <h2 className={styles.title}>About</h2>
-        <div className={styles.content}>
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img
+          className={styles.aboutImage}
+          src={getImageUrl("about/star.png")}
+          alt="About Image"
+        />
+
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/web.png")} alt="web design icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Web Design</h3>
+              <p>
+                Our expert team of designers creates stunning, user-friendly
+                websites that capture your brand essence and engage your
+                audience.
+              </p>
+            </div>
+          </li>
+
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/settings.png")} alt="settings icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Search Engine Optimization (SEO)</h3>
+              <p>
+                We optimize your website to rank higher in search engine
+                results, driving organic traffic and increasing your visibility
+                online.
+              </p>
+            </div>
+          </li>
+
+          <li className={styles.aboutItem}>
             <img
-            className={styles.aboutImage} 
-            src={getImageUrl('about/star.png')} 
-            alt='About Image' 
+              src={getImageUrl("about/digitalMarketing.png")}
+              alt="digitalMarketing icon"
             />
+            <div className={styles.aboutItemText}>
+              <h3>Digital Marketing</h3>
+              <p>
+                Our social media experts manage your social media accounts and
+                execute digital marketing strategies designed to attract,
+                convert, retain customers and maximize your ROI.
+              </p>
+            </div>
+          </li>
 
-            <ul className={styles.aboutItems}>
-                <li className={styles.aboutItem}>
-                <img src={getImageUrl('about/web.png')} alt='web design icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Web Design</h3>
-                    <p>Our expert team of designers creates stunning, 
-                    user-friendly websites that capture your brand essence and engage your audience.
-                    </p>
-                </div>
-                </li>
-
-
-                <li className={styles.aboutItem}>
-                <img src={getImageUrl('about/settings.png')} alt='settings icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Search Engine Optimization (SEO)</h3>
-                    <p>
-                    We optimize your website to rank higher in search engine results, 
-                    driving organic traffic and increasing your visibility online. 
-                    </p>
-                </div>
-                </li>
-
-                <li className={styles.aboutItem}>
-                <img src={getImageUrl('about/digitalMarketing.png')} alt='digitalMarketing icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Digital Marketing</h3>
-                    <p>
-                    Our digital marketing strategies are designed to attract, convert and retain customers, 
-                    utilizing the latest tools and techniques to maximize your ROI.
-                    </p>
-                </div>
-                </li>
-
-
-                <li className={styles.aboutItem}>
-                <img src={getImageUrl('about/wand.png')} alt='branding icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Branding</h3>
-                    <p>
-                    We help you establish a strong and memorable brand identity, 
-                    ensuring consistency across all your marketing channels.
-                    </p>
-                </div>
-                </li>
-
-                <li className={styles.aboutItem}>
-                <img src={getImageUrl('about/chat.png')} alt='chat icon'/>
-                <div className={styles.aboutItemText}>
-                    <h3>Social Media Management</h3>
-                    <p>
-                    Our social media experts manage your social media accounts, creating engaging 
-                    content and fostering meaningful interactions with your audience.
-                    </p>
-                </div>
-                </li>
-
-            </ul>
-        </div>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/wand.png")} alt="branding icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Branding</h3>
+              <p>
+                We help you establish a strong and memorable brand identity,
+                ensuring consistency across all your marketing channels.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default About;
--- neostratify/src/Components/Hero/Hero.jsx
+++ neostratify/src/Components/Hero/Hero.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
-import { getImageUrl } from '../../utils';
-import styles from './Hero.module.css';
+import React from "react";
+import { getImageUrl } from "../../utils";
+import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Unlock the full potential of your business and stand out in an ever evolving world.</h1>
-            <p className={styles.description}> 
-            We help businesses establish a strong brand identity, credibility, boost online presence 
-            and growth through our comprehensive suite of innovative solutions and business tool-kit.
-            </p>
-            <a className={styles.contactBtn} href='mailto:daniel.jonesobot@gmail.com'>Get in touch</a>
-        </div>
-        <img className={styles.heroImg} src={getImageUrl('hero/triangle.png')} alt='Triangle Image'/>
-        <div className={styles.topBlur} />
-        <div className={styles.bottomBlur}/>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          Unlock the full potential of your business and stand out in an ever
+          evolving world.
+        </h1>
+        <p className={styles.description}>
+          We help businesses establish a strong brand identity, credibility,
+          boost online presence and growth through our comprehensive suite of
+          innovative solutions and business tool-kit.
+        </p>
+        <a
+          className={styles.contactBtn}
+          href="mailto:daniel.jonesobot@gmail.com"
+        >
+          Get in touch
+        </a>
+      </div>
+      <img
+        className={styles.heroImg}
+        src={getImageUrl("hero/triangle.png")}
+        alt="Triangle Image"
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
-  )
-}
+  );
+};
 
 export default Hero;
