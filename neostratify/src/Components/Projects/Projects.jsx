1722737b1821ac0bef0c844716451daab76a3b78
danieljonesobot <daniel.jonesobot@gmail.com>
2024-03-27 23:42:44 +0100

    created project component for projects section

--- neostratify/src/Components/Projects/Projects.jsx
+++ neostratify/src/Components/Projects/Projects.jsx
@@ -1,29 +1,40 @@
 import React from "react";
 import projects from "../../data/projects.json";
 import { getImageUrl } from "../../utils";
+import styles from "./Projects.module.css";
 
 const Projects = () => {
   return (
-    <section>
-      <h2>Projects</h2>
-      <div>
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
         {projects.map((project, index) => {
           return (
-            <div key={index}>
+            <div className={styles.contain} key={index}>
               <img
+                className={styles.image}
                 src={getImageUrl(project.imageSrc)}
                 alt={`image of ${project.title}`}
               />
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <ul>
+              <h3 className={styles.till}>{project.title}</h3>
+              <p className={styles.description}>{project.description}</p>
+              <ul className={styles.skills}>
                 {project.skills.map((skill, index) => {
-                  return <li key={index}>{skill}</li>;
+                  return (
+                    <li className={styles.skill} key={index}>
+                      {skill}
+                    </li>
+                  );
                 })}
               </ul>
 
-              <div>
-                <a href={project.demo}></a>
+              <div className={styles.links}>
+                <a className={styles.link} href={project.demo}>
+                  Demo
+                </a>
+                <a className={styles.link} href="#">
+                  {project.service}
+                </a>
               </div>
             </div>
           );
