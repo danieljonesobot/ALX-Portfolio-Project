99a02c3fe9a023fc8f80e93661d44ac5cfc671fa
danieljonesobot <daniel.jonesobot@gmail.com>
2024-03-27 23:41:38 +0100

    created contact component

---
+++ neostratify/src/Components/Contact/Contact.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Contact.module.css";
+
+const Contact = () => {
+  return <section className={styles.container}></section>;
+};
+
+export default Contact;
