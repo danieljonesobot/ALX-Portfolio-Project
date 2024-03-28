daec370f6067074ee1b41e76beab4a622f4ed3e9
danieljonesobot <daniel.jonesobot@gmail.com>
2024-03-28 18:07:11 +0100

    created classes and added styling for contact component

--- neostratify/src/Components/Contact/Contact.jsx
+++ neostratify/src/Components/Contact/Contact.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
 
 const Contact = () => {
-  return <section className={styles.container}></section>;
+  return (
+    <footer className={styles.container} id="contact">
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Get in touch with us</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon1.png")} alt="Email Icon" />
+          <a href="mailto:info@neostratify.com.ng">info@neostratify.com.ng</a>
+        </li>
+
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/phoneIcon.png")} alt="Phone Icon" />
+          <a href="#">+23470670xxxxx</a>
+        </li>
+
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/whatsappIcon.png")}
+            alt="WhatsApp Icon"
+          />
+          <a href="mailto:info@neostratify.com.ng">Whatsapp</a>
+        </li>
+      </ul>
+    </footer>
+  );
 };
 
 export default Contact;
