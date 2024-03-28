5e472d4c5e75ed37d4e0565a724dd50f4a645f7f
danieljonesobot <daniel.jonesobot@gmail.com>
2024-03-28 18:02:41 +0100

    updated nav links

--- neostratify/src/Components/Navbar/Navbar.jsx
+++ neostratify/src/Components/Navbar/Navbar.jsx
@@ -37,10 +37,10 @@ const Navbar = () => {
             <a href="#expertise">Expertise</a>
           </li>
           <li>
-            <a href="#solutions">Solutions</a>
+            <a href="#projects">Projects</a>
           </li>
           <li>
-            <a href="#projects">Projects</a>
+            <a href="#solutions">Solutions</a>
           </li>
           <li>
             <a href="#contact">Contact</a>
